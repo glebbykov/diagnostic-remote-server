ee83704be8058ca219464ebe47b599d4a7ddf9a6
glebbykov <glebbykov1993@gmail.com>
2023-01-24 07:45:28 +0000

    working vers 0.15

--- diag.py
+++ diag.py
@@ -2,6 +2,29 @@ import tkinter as tk
 import paramiko
 
 class DiagnFrame(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.title("Server Diagn Tool")
+
+        # Create widgets
+        host_label = tk.Label(self, text="Host: ")
+        host_label.pack()
+        self.host_input = tk.Entry(self)
+        self.host_input.pack()
+
+        username_label = tk.Label(self, text="Username: ")
+        username_label.pack()
+        self.username_input = tk.Entry(self)
+        self.username_input.pack()
+
+        password_label = tk.Label(self, text="Password: ")
+        password_label.pack()
+        self.password_input = tk.Entry(self, show="*")
+        self.password_input.pack()
+
+        diagnose_button = tk.Button(self, text="Diagnose", command=self.diagnose)
+        diagnose_button.pack()
+
     def diagnose(self):
         host = self.host_input.get()
         user = self.username_input.get()
@@ -14,15 +37,34 @@ class DiagnFrame(tk.Tk):
             ssh.connect(host, username=user, password=password)
             stdin, stdout, stderr = ssh.exec_command("uptime")
             uptime = stdout.read().decode("utf-8")
-            print("Uptime: ", uptime.strip())
 
             stdin, stdout, stderr = ssh.exec_command("df -h /")
             disk_space = stdout.read().decode("utf-8")
-            print("Disk Space: \n", disk_space)
+
+            #stdin, stdout, stderr = ssh.exec_command("cat /etc/os-release | grep 'PRETTY_NAME'| cut -d'=' -f2")
+            #os_version = stdout.read().decode("utf-8")
+            #os_version = os_version.strip().split("\n")
+            #os_version = os_version.strip().split("\n")
+            #os_version = " ".join([i.strip().strip('"') for i in os_version])
 
             stdin, stdout, stderr = ssh.exec_command("cat /etc/os-release | grep 'PRETTY_NAME'| cut -d'=' -f2")
             os_version = stdout.read().decode("utf-8")
-            print("OS version: \n", os_version.strip())
+            os_version = os_version.strip().split("\n")
+            os_version = " ".join([i.strip().strip('"') for i in os_version])
+            print("OS version: ", os_version)
+
+            self.result_window = tk.Toplevel(self)
+            self.result_window.title("Diagnostic Result")
+
+            uptime_label = tk.Label(self.result_window, text="Uptime: " + uptime.strip())
+            uptime_label.pack()
+
+            disk_space_label = tk.Label(self.result_window, text="Disk Space: \n" + disk_space)
+            disk_space_label.pack()
+
+            os_version_label = tk.Label(self.result_window, text="OS version: " + os_version)
+            os_version_label.pack()
+
         except paramiko.ssh_exception.AuthenticationException as e:
             print("Failed to connect: Invalid credentials")
         except paramiko.ssh_exception.SSHException as e:
