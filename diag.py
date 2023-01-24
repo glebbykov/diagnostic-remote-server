fb85b4032d71b0ed7364da7de554fb91f87a28ad
glebbykov <glebbykov1993@gmail.com>
2023-01-24 10:03:31 +0000

    add diagnostic_result.txt

--- diag.py
+++ diag.py
@@ -36,15 +36,15 @@ class DiagnFrame(tk.Tk):
         try:
             ssh.connect(host, username=user, password=password)
             stdin, stdout, stderr = ssh.exec_command("uptime")
-            uptime = stdout.read().decode("utf-8")
+            self.uptime = stdout.read().decode("utf-8")
 
             stdin, stdout, stderr = ssh.exec_command("df -h /")
-            disk_space = stdout.read().decode("utf-8")
+            self.disk_space = stdout.read().decode("utf-8")
 
             stdin, stdout, stderr = ssh.exec_command("cat /etc/os-release | grep 'PRETTY_NAME'| cut -d'=' -f2")
             os_version = stdout.read().decode("utf-8")
             os_version = os_version.strip().split("\n")
-            os_version = " ".join([i.strip().strip('"') for i in os_version])
+            self.os_version = " ".join([i.strip().strip('"') for i in os_version])
 
             self.result_window = tk.Toplevel(self)
             self.result_window.title("Diagnostic Result")
@@ -52,14 +52,17 @@ class DiagnFrame(tk.Tk):
             host_label = tk.Label(self.result_window, text="Host: " + host.strip())
             host_label.pack()
 
-            uptime_label = tk.Label(self.result_window, text="Uptime: " + uptime.strip())
+            uptime_label = tk.Label(self.result_window, text="Uptime: " + self.uptime.strip())
             uptime_label.pack()
 
-            disk_space_label = tk.Label(self.result_window, text="Disk Space: \n" + disk_space)
+            disk_space_label = tk.Label(self.result_window, text="Disk Space: \n" + self.disk_space)
             disk_space_label.pack()
 
-            os_version_label = tk.Label(self.result_window, text="OS version: " + os_version)
+            os_version_label = tk.Label(self.result_window, text="OS version: " + self.os_version)
             os_version_label.pack()
+
+            save_button = tk.Button(self.result_window, text="Save Result", command=self.save_result)
+            save_button.pack()
 
         except paramiko.ssh_exception.AuthenticationException as e:
             print("Failed to connect: Invalid credentials")
@@ -87,6 +90,17 @@ class DiagnFrame(tk.Tk):
         diagnose_button = tk.Button(self, text="Diagnose", command=self.diagnose)
         diagnose_button.pack()
 
+    def save_result(self):
+        host = self.host_input.get()
+        uptime = self.uptime.strip()
+        disk_space = self.disk_space.strip()
+        os_version = self.os_version.strip()
+
+        with open("diagnostic_result.txt", "w") as f:
+            f.write("Host: " + host + "\n")
+            f.write("Uptime: " + uptime + "\n")
+            f.write("Disk Space: " + disk_space + "\n")
+            f.write("OS version: " + os_version + "\n")
 
 app = DiagnFrame()
 app.mainloop()
