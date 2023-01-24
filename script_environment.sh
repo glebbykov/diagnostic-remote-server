e556fdd6c7439546fe67fcce766aaf377f3dbea5
glebbykov <122178014+glebbykov@users.noreply.github.com>
2023-01-24 09:06:18 +0300

    Create script_environment.sh

---
+++ script_environment.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo yum -y update
+
+sudo yum -y install python3
+
+sudo pip3 install --upgrade pip
+
+sudo yum -y install python3 python3-pip
+
+sudo yum -y install python3-tkinter
+
+sudo pip3 install paramiko
+
+yum -y groupinstall "GNOME Desktop" "Graphical Administration Tools"
+
+ln -sf /lib/systemd/system/runlevel5.target /etc/systemd/system/default.target
+
+echo 'perform a reboot as soon as you are ready'
