28b51bf7c15364796767d8be64c593e88fa31432
glebbykov <122178014+glebbykov@users.noreply.github.com>
2023-01-24 18:48:04 +0300

    Update and rename script_environment.sh to script_environment_for_centos7.sh

--- script_environment_for_centos7.sh
+++ script_environment_for_centos7.sh
@@ -8,12 +8,12 @@ sudo pip3 install --upgrade pip
 
 sudo yum -y install python3 python3-pip
 
+#Installing the required libraries
 sudo yum -y install python3-tkinter
-
 sudo pip3 install paramiko
 
+#installing the GNOME graphical interface requires restarting the system
 yum -y groupinstall "GNOME Desktop" "Graphical Administration Tools"
-
 ln -sf /lib/systemd/system/runlevel5.target /etc/systemd/system/default.target
 
 echo 'perform a reboot as soon as you are ready'
